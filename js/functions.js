864d03224c3aa05232063c0808fba8eb6b150016
smashplays <jesusterres1999@gmail.com>
2022-11-19 20:31:28 +0100

    Fixed bugs JS

--- js/functions.js
+++ js/functions.js
@@ -33,10 +33,11 @@ function deleteTable() {
 }
 
 function generateTable(data) {
+    deleteTable();
     const table = document.querySelector('tbody');
     for (let i = 0; i < data.length; i++) {
         let row = `<tr id="row${i}" class="row">
-                        <td><button id="${i}" class="button" onclick="removeRow(this)">X</button><button id="showModal" onclick="editRow(this, ${i})">Edit</button></td>
+                        <td><button id="${i}" class="button" onclick="removeRow(${i})">X</button><button id="showModal" onclick="editRow(this, ${i})">Edit</button></td>
 						<td id="name${i}">${data[i].name}</td>
 						<td id="description${i}">${data[i].description}</td>
 						<td id="serial${i}">${data[i].serial}</td>
@@ -49,99 +50,8 @@ function generateTable(data) {
 
 
 function removeRow(button) {
-    const number = button.id;
-    const row = document.getElementById('row' + number);
+    const row = document.getElementById('row' + button);
     row.remove();
-}
-
-function editRow(button, number) {
-    const formModal = document.querySelector('#formModal');
-    const closeBtn = document.querySelector('#closeBtn');
-
-    const selectedRow = button.parentElement.parentElement;
-
-    formModal.classList.add('showModal');
-
-    closeBtn.addEventListener('click', () => {
-        formModal.classList.remove('showModal');
-    });
-
-    // Valores al abrir el formulario
-    objectArray[number].name = selectedRow.cells[1].innerHTML;
-    objectArray[number].description = selectedRow.cells[2].innerHTML;
-    objectArray[number].serial = selectedRow.cells[3].innerHTML;
-
-    document.getElementById("name").value = objectArray[number].name;
-    document.getElementById("description").value = objectArray[number].description;
-    document.getElementById("serial").value = objectArray[number].serial;
-
-    const active = document.getElementById('status');
-
-    if (selectedRow.cells[4].innerHTML === 'Activo') {
-        objectArray[number].status = 'Activo';
-        active.checked = true;
-    } else {
-        objectArray[number].status = 'Inactivo';
-        active.checked = false;
-    }
-
-    const low = document.getElementById('low');
-    const medium = document.getElementById('medium');
-    const high = document.getElementById('high');
-
-    low.checked = false;
-    medium.checked = false;
-    high.checked = false;
-
-    if (selectedRow.cells[5].innerHTML === 'Alta') {
-        objectArray[number].priority = 'Alta';
-        high.checked = true;
-    } else if (selectedRow.cells[5].innerHTML === 'Media') {
-        objectArray[number].priority = 'Media';
-        medium.checked = true;
-    } else if (selectedRow.cells[5].innerHTML === 'Baja') {
-        objectArray[number].priority = 'Baja';
-        low.checked = true;
-    }
-
-    const saveData = document.querySelector('#saveData');
-
-    // Evento tras guardar los datos
-    saveData.addEventListener('click', () => {
-
-        // Asignamos los valores del formulario al objeto y luego metemos esos valores a las celdas de la tabla
-        objectArray[number].name = document.getElementById("name").value;
-        objectArray[number].description = document.getElementById("description").value;
-        objectArray[number].serial = document.getElementById("serial").value;
-
-        selectedRow.cells[1].innerHTML = objectArray[number].name;
-        selectedRow.cells[2].innerHTML = objectArray[number].description;
-        selectedRow.cells[3].innerHTML = objectArray[number].serial;
-
-        if (active.checked) {
-            objectArray[number].status = 'Activo';
-            selectedRow.cells[4].innerHTML = objectArray[number].status;
-        } else {
-            objectArray[number].status = 'Inactivo';
-            selectedRow.cells[4].innerHTML = objectArray[number].status;
-        }
-
-        if (low.checked) {
-            objectArray[number].priority = 'Baja';
-            selectedRow.cells[5].innerHTML = objectArray[number].priority;
-        } else if (medium.checked) {
-            objectArray[number].priority = 'Media';
-            selectedRow.cells[5].innerHTML = objectArray[number].priority;
-        } else if (high.checked) {
-            objectArray[number].priority = 'Alta';
-            selectedRow.cells[5].innerHTML = objectArray[number].priority;
-        } else {
-            objectArray[number].priority = 'Baja';
-            selectedRow.cells[5].innerHTML = objectArray[number].priority;
-        }
-
-        formModal.classList.remove('showModal');
-    });
 }
 
 function filterTable() {
@@ -162,10 +72,86 @@ function filterTable() {
                 data.priority.toLowerCase().includes(search)
             );
         });
-        deleteTable();
         generateTable(filteredObjects);
     } else {
-        deleteTable();
         generateTable(objectArray);
     }
+}
+
+function editRow(button, number) {
+    const formModal = document.querySelector('#formModal');
+    const closeBtn = document.querySelector('#closeBtn');
+
+    selectedRow = button.parentElement.parentElement;
+
+    formModal.classList.add('showModal');
+
+    closeBtn.addEventListener('click', () => {
+        formModal.classList.remove('showModal');
+    });
+
+    // Valores al abrir el formulario
+
+    document.getElementById("name").value = objectArray[number].name;
+    document.getElementById("description").value = objectArray[number].description;
+    document.getElementById("serial").value = objectArray[number].serial;
+
+    const active = document.getElementById('status');
+
+    if (objectArray[number].status === 'Activo') {
+        active.checked = true;
+    } else {
+        active.checked = false;
+    }
+
+    const low = document.getElementById('low');
+    const medium = document.getElementById('medium');
+    const high = document.getElementById('high');
+
+    low.checked = false;
+    medium.checked = false;
+    high.checked = false;
+
+    if (objectArray[number].priority === 'Alta') {
+        high.checked = true;
+    } else if (objectArray[number].priority === 'Media') {
+        medium.checked = true;
+    } else if (objectArray[number].priority === 'Baja') {
+        low.checked = true;
+    }
+
+    const saveData = document.querySelector('#saveData');
+
+    // Evento tras guardar los datos
+    saveData.addEventListener('click', () => {
+
+        // Asignamos los valores del formulario al objeto y luego metemos esos valores a las celdas de la tabla
+        selectedRow.cells[1].innerText = document.getElementById("name").value;
+        selectedRow.cells[2].innerText = document.getElementById("description").value;
+        selectedRow.cells[3].innerText = document.getElementById("serial").value;
+
+        if (active.checked) {
+            selectedRow.cells[4].innerText = 'Activo';
+        } else {
+            selectedRow.cells[4].innerText = 'Inactivo';
+        }
+
+        if (low.checked) {
+            selectedRow.cells[5].innerText = 'Baja';
+        } else if (medium.checked) {
+            selectedRow.cells[5].innerText = 'Media';
+        } else if (high.checked) {
+            selectedRow.cells[5].innerText = 'Alta';
+        } else {
+            selectedRow.cells[5].innerText = 'Baja';
+        }
+
+        objectArray[number].name = selectedRow.cells[1].innerText;
+        objectArray[number].description = selectedRow.cells[2].innerText;
+        objectArray[number].serial = selectedRow.cells[3].innerText;
+        objectArray[number].status = selectedRow.cells[4].innerText;
+        objectArray[number].priority = selectedRow.cells[5].innerText;
+
+        formModal.classList.remove('showModal');
+    });
 }
--- js/nav.js
+++ js/nav.js
@@ -27,22 +27,22 @@ function insertHtmlXml() {
 }
 
 function checkNavElement() {
-    const item = document.getElementsByClassName('item');
+    const form = document.getElementById('formItem');
+    const table = document.getElementById('tableItem');
+    const gallery = document.getElementById('galleryItem');
+    const definition = document.getElementById('definitionItem');
+    const about_us = document.getElementById('aboutUsItem');
+
 
     if (window.location.href.includes('form')) {
-        const form = document.getElementById('formItem');
         form.style = 'background-color: white;';
     } else if (window.location.href.includes('table')) {
-        const table = document.getElementById('tableItem');
         table.style = 'background-color: white;';
     } else if (window.location.href.includes('gallery')) {
-        const gallery = document.getElementById('galleryItem');
         gallery.style = 'background-color: white;';
     } else if (window.location.href.includes('definition')) {
-        const definition = document.getElementById('definitionItem');
         definition.style = 'background-color: white;';
     } else if (window.location.href.includes('aboutus')) {
-        const about_us = document.getElementById('aboutUsItem');
         about_us.style = 'background-color: white;';
     }
 }
